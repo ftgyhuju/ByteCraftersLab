5de9073b1c6695ae089de4521c8c9287b5ca57f6
ftgyhuju <gs1crain360@hotmail.com>
2024-05-14 03:01:54 +0700

    update task

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-function removeNthFromEnd(head, n) {
-  const dummy = new ListNode(0);
-  dummy.next = head;
-  let first = dummy;
-  let second = dummy;
-  for (let i = 0; i <= n; i++) {
-    first = first.next;
-  }
-  while (first !== null) {
-    first = first.next;
-    second = second.next;
-  }
-  second.next = second.next.next;
-  return dummy.next;
-}
+const originalObject = { name: "Alice", age: 30 };
+const clonedObject = JSON.parse(JSON.stringify(originalObject));
+console.log(clonedObject); // { name: "Alice", age: 30 }
